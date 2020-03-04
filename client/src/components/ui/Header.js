915596ddf3ed6edc09a01cc97760f3b990281d5b
Vuthy <suonx002@gmail.com>
2020-03-04 12:30:34 -0600

    add drawer menu for mobile devices

--- client/src/components/ui/Header.js
+++ client/src/components/ui/Header.js
@@ -8,8 +8,13 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
-
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const ElevationScroll = props => {
   const { children } = props;
@@ -27,10 +32,11 @@ const ElevationScroll = props => {
 const useStyles = makeStyles(theme => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
-    marginBottom: '3em'
+    marginBottom: '2em'
   },
   appbar: {
-    padding: '0.5rem 0'
+    padding: '0.5rem 0',
+    zIndex: theme.zIndex.modal + 1
   },
   logoContainer: {
     fontSize: '1.5rem',
@@ -47,27 +53,145 @@ const useStyles = makeStyles(theme => ({
     ...theme.typography.tab,
     minWidth: 10,
     marginLeft: 25
+  },
+  drawerIconContainer: {
+    marginLeft: 'auto'
+  },
+  drawerIcon: {
+    height: 50,
+    width: 50,
+    color: theme.palette.common.white
+  },
+
+  drawer: {
+    backgroundColor: theme.palette.common.blue
+  },
+  drawerItem: {
+    ...theme.typography.tab,
+    color: 'white',
+    opacity: '0.8'
+  },
+  drawerItemSelected: {
+    '& .MuiListItemText-root': {
+      opacity: 1
+    }
+  },
+  listContainer: {
+    width: 200
+  },
+  listItem: {
+    textAlign: 'center'
   }
 }));
 
 const Header = () => {
   const classes = useStyles();
+  const theme = useTheme();
+  const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
 
+  const matches = useMediaQuery(theme.breakpoints.down('md'));
+
+  const [openDrawer, setOpenDrawer] = useState(false);
   const [value, setValue] = useState(0);
 
   const handleChange = (e, newValue) => {
     setValue(newValue);
   };
 
-  useEffect(() => {
-    if (window.location.pathname === '/' && value !== 0) {
-      setValue(0);
-    } else if (window.location.pathname === '/register' && value !== 1) {
-      setValue(1);
-    } else if (window.location.pathname === '/login' && value !== 2) {
-      setValue(2);
+  const routes = [
+    {
+      name: 'Home',
+      link: '/',
+      activeIndex: 0
+    },
+    {
+      name: 'Register',
+      link: '/register',
+      activeIndex: 1
+    },
+    {
+      name: 'Login',
+      link: '/login',
+      activeIndex: 2
     }
-  });
+  ];
+
+  useEffect(() => {
+    [...routes].forEach(route => {
+      switch (window.location.pathname) {
+        case `${route.link}`:
+          if (value !== route.activeIndex) {
+            setValue(route.activeIndex);
+          }
+          break;
+        default:
+          break;
+      }
+    });
+  }, [value, routes]);
+
+  const tabs = (
+    <Tabs
+      className={classes.tabContainer}
+      value={value}
+      onChange={handleChange}
+      indicatorColor="primary"
+    >
+      {routes.map((route, index) => (
+        <Tab
+          key={`${route}-${index}`}
+          className={classes.tab}
+          label={route.name}
+          component={Link}
+          to={route.link}
+        />
+      ))}
+    </Tabs>
+  );
+
+  const drawer = (
+    <Fragment>
+      <SwipeableDrawer
+        disableBackdropTransition={!iOS}
+        disableDiscovery={iOS}
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+        onOpen={() => setOpenDrawer(true)}
+        classes={{ paper: classes.drawer }}
+      >
+        <div className={classes.toolbarMargin} />
+        <List disablePadding className={classes.listContainer}>
+          {routes.map((route, index) => (
+            <ListItem
+              className={classes.listItem}
+              key={`${route}~${index}`}
+              divider
+              button
+              component={Link}
+              to={route.link}
+              selected={value === route.activeIndex}
+              classes={{ selected: classes.drawerItemSelected }}
+              onClick={() => {
+                setOpenDrawer(false);
+                setValue(route.activeIndex);
+              }}
+            >
+              <ListItemText className={classes.drawerItem} disableTypography>
+                {route.name}
+              </ListItemText>
+            </ListItem>
+          ))}
+        </List>
+      </SwipeableDrawer>
+      <IconButton
+        onClick={() => setOpenDrawer(!openDrawer)}
+        className={classes.drawerIconContainer}
+      >
+        <MenuIcon className={classes.drawerIcon} />
+      </IconButton>
+    </Fragment>
+  );
+
   return (
     <Fragment>
       <ElevationScroll>
@@ -82,31 +206,7 @@ const Header = () => {
             >
               Web Resources
             </Button>
-            <Tabs
-              className={classes.tabContainer}
-              value={value}
-              onChange={handleChange}
-              indicatorColor="primary"
-            >
-              <Tab
-                className={classes.tab}
-                label="Home"
-                component={Link}
-                to="/"
-              />
-              <Tab
-                className={classes.tab}
-                label="Register"
-                component={Link}
-                to="/register"
-              />
-              <Tab
-                className={classes.tab}
-                label="Login"
-                component={Link}
-                to="/login"
-              />
-            </Tabs>
+            {matches ? drawer : tabs}
           </Toolbar>
         </AppBar>
       </ElevationScroll>
