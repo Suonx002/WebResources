a7ac77f8aed6883c4adc00520dd830c6d46b2de5
Vuthy <suonx002@gmail.com>
2020-03-18 21:44:40 -0500

    fix status code error from 500 to 401 for login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -56,7 +56,7 @@ exports.login = catchAsync(async (req, res, next) => {
   const user = await User.findOne({ email }).select('+password');
 
   if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(new AppError('Invalid email or password'), 401);
+    return next(new AppError('Invalid email or password', 401));
   }
 
   createSendToken(user, 200, req, res);
