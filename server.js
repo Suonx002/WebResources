23120bab3ce75b65560321191fc579a902fbd270
Vuthy <suonx002@gmail.com>
2020-03-21 11:03:56 -0500

    trying to fix uploads image on heroku

--- server.js
+++ server.js
@@ -47,12 +47,14 @@ app.use('/api/v1/comments', commentRouter);
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+  // Set static folder for react
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // set statis folder for uploads
+  app.use(express.static(path.join(__dirname, 'uploads')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  // app.get('*', (req, res) =>
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  // );
 }
 
 // catach errors (all verbs: get post put patch ,etc.)
