f9a45b6d3ec2920a0206beafe263d507b2e2cbb8
Vuthy <suonx002@gmail.com>
2020-03-23 23:51:50 -0500

    change menu to right side

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -93,7 +93,7 @@ const useStyles = makeStyles(theme => ({
     }
   },
   listContainer: {
-    width: 200
+    width: 250
   },
   listItem: {
     textAlign: 'center'
@@ -237,6 +237,7 @@ const Header = props => {
         disableBackdropTransition={!iOS}
         disableDiscovery={iOS}
         open={openDrawer}
+        anchor={'right'}
         onClose={() => setOpenDrawer(false)}
         onOpen={() => setOpenDrawer(true)}
         classes={{ paper: classes.drawer }}
